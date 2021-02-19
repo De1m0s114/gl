9dbcce7ea7109f91ce269f965f2d6a468df3b4fd
De1m0s114 <yegor1002@gmail.com>
2021-02-20 01:55:19 +0200

    final test

--- test_list.py
+++ test_list.py
@@ -3,10 +3,8 @@ import unittest
 def rem_last_element(list):
     list.remove(list[-1])
     return list
+
 class TestListMethods(unittest.TestCase):
-    def test_Yegor_Panasuk_FI94(self):
-        list=[1, 2, 3, 4, 5, 6]
-        self.assertEqual(rem_last_element(list),[1, 2, 3, 4, 5])
 
     def test_roman_tkalenko_fi_13(self):
         self.assertEqual(len([]), 0)
@@ -16,5 +14,12 @@ class TestListMethods(unittest.TestCase):
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
 
-if name == 'main':
+       
+    def test_Yegor_Panasuk_FI94(self):
+        list=[1, 2, 3, 4, 5, 6]
+        self.assertEqual(rem_last_element(list),[1, 2, 3, 4, 5])
+
+
+if __name__ == '__main__':
     unittest.main()
+
