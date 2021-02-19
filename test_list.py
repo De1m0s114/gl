959283fc6886186a990229dbbcaf96b50cb3748f
De1m0s114 <yegor1002@gmail.com>
2021-02-19 23:42:13 +0200

    add test for remove with '#' will be ignored, and an empty message aborts the commit.

--- test_list.py
+++ test_list.py
@@ -2,6 +2,9 @@ import unittest
 
 
 class TestListMethods(unittest.TestCase):
+    def test_Yegor_Panasuk_FI94(self):
+        list=[1, 2, 3, 4, 5, 6]
+        self.assertEqual(rem_last_element(list),[1, 2, 3, 4, 5])
 
     def test_roman_tkalenko_fi_13(self):
         self.assertEqual(len([]), 0)
@@ -11,9 +14,5 @@ class TestListMethods(unittest.TestCase):
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
 
-    def test_illia_kripaka_fi_94_2(self):
-        self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
-
-
-if __name__ == '__main__':
+if name == 'main':
     unittest.main()
