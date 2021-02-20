ac3dc3230b88daf644b96c75946a57cba3ea0d9e
De1m0s114 <61823460+De1m0s114@users.noreply.github.com>
2021-02-20 11:24:47 +0200

    Update test_list.py

--- test_list.py
+++ test_list.py
@@ -16,7 +16,6 @@ class TestListMethods(unittest.TestCase):
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
 
-
     def test_Yegor_Panasuk_FI94(self):
         list = [1, 2, 3, 4, 5, 6]
         self.assertEqual(rem_last_element(list), [1, 2, 3, 4, 5])
@@ -27,6 +26,6 @@ class TestListMethods(unittest.TestCase):
     def test_illia_kripaka_fi_94_2(self):
         self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
 
+        
 if __name__ == '__main__':
     unittest.main()
-
